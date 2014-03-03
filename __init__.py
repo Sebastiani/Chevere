bc3ccd993b96a1ff466fed9a2e95ffb94c09d8b7
Sebastiani <sebastiani.aguirre@upr.edu>
2014-03-03 16:26:03 -0500

    More html files

--- __init__.py
+++ __init__.py
@@ -7,23 +7,10 @@ from flask.ext.sqlalchemy import SQLAlchemy
 #----------------------------------------
 
 app = Flask(__name__)
-app.config.from_object('config')
+#app.config.from_object('Chevere/config.py')
 app.config.update(DEBUG = True)
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
 
 from Chevere import views, models
-#----------------------------------------
-# controllers
-#----------------------------------------
 
-@app.route("/")
-def hello():
-    return "Hello from Python!"
 
-#----------------------------------------
-# launch
-#----------------------------------------
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
--- views.py
+++ views.py
@@ -1,3 +1,9 @@
 from flask import render_template, flash, redirect
 from Chevere import app
 from forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+	return render_template("home.html")
