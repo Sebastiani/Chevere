b4962a12c9df619e23c99c2482563f9bfda874a2
Sebastiani <sebastiani.aguirre@upr.edu>
2014-03-03 22:14:19 -0500

    fancying stuff up

--- models.py
+++ models.py
@@ -1,3 +1,17 @@
-from flask import render_template, flash, redirect
-from Chevere import app
-from forms import LoginForm
+from Chevere import db
+
+ROLE_USER = 0
+ROLE_ADMIN = 1
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	name =  db.Column(db.String(120), index = True, unique = True)
+	email = db.Column(db.String(120), index = True, unique = True)
+	gender = db.Column(db.String(1), index = True, unique = False)
+	fav_food =  db.Column(db.String(50), index = True, unique = False)
+	about = db.Column(db.String(300), index = True, unique = False)
+	date_joined =  db.Column(db.String(8), index = True, unique =  False)
+	role =  db.Column(db.SmallInteger, default = ROLE_USER)
+
+	def __repr__(self):
+		return '<User %r>' % (self.name)
--- views.py
+++ views.py
@@ -2,8 +2,22 @@ from flask import render_template, flash, redirect
 from Chevere import app
 from forms import LoginForm
 
+user =  {'name': 'Sebastiani', 'lastname':'Aguirre'}
+
+
+@app.route('/home')
+def index():
+	return render_template("home.html", user=user)
+
 
 @app.route('/')
-@app.route('/index')
-def index():
-	return render_template("home.html")
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+		return redirect('/home')
+
+	return render_template('login.html',
+		form = form,
+		providers =  app.config['OPENID_PROVIDERS'])
