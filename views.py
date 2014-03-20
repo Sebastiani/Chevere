64da97dd584c40445ec77dbd3b1c41413b6dc101
Sebastiani <sebastiani.aguirre@upr.edu>
2014-03-19 21:01:31 -0400

    Update views.py

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
-from flask import render_template, flash, redirect
-from Chevere import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from Chevere import app, db, lm, oid
 from forms import LoginForm
+from models import User, ROLE_USER, ROLE_ADMIN
 
 user =  {'name': 'Sebastiani', 'lastname':'Aguirre'}
 
@@ -12,12 +14,20 @@ def index():
 
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+	if g.user is not None and g.user.is_authenticated():
+		return redirect(url_for('index'))
+		
 	form = LoginForm()
 	if form.validate_on_submit():
-		flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-		return redirect('/home')
+		session['remember_me'] =  form.remember_me.data
+		return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
 
 	return render_template('login.html',
 		form = form,
 		providers =  app.config['OPENID_PROVIDERS'])
+		
+@lm.user_loader
+def load_user(id):
+	return User.query.get(int(id))
