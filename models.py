43b170d2edfd05bd3ed9b67a2595966e9d7e6b05
Sebastiani <sebastiani.aguirre@upr.edu>
2014-03-19 20:54:13 -0400

    Update models.py

--- models.py
+++ models.py
@@ -12,7 +12,15 @@ class User(db.Model):
 	about = db.Column(db.String(300), index = True, unique = False)
 	date_joined =  db.Column(db.String(8), index = True, unique =  False)
 	role =  db.Column(db.SmallInteger, default = ROLE_USER)
-
+	
+	def is_authenticated(self):
+		return True
+	def is_active(self):
+		return True
+	def is_anonymous(self):
+		return False
+	def get_id(self):
+		return unicode(self.id)
 	def __repr__(self):
 		return '<User %r>' % (self.name)
 		
